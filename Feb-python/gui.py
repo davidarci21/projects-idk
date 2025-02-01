dd678e0db4f022650cfc2416dc647d7dad6fd490
David Arciniegas <davidarci2103@gmail.com>
2025-01-31 18:53:42 -0600

    creation of GUI 1st step

---
+++ Feb-python/gui.py
@@ -0,0 +1,24 @@
+from tkinter import *
+import customtkinter
+
+#theme and color
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+#app itself
+app = customtkinter.CTk()
+app.geometry("600x600")
+app.title("Titulo")
+app.resizable(width=False, height=False)
+
+
+#root
+root = customtkinter.CTk()
+
+#
+
+
+
+
+app.mainloop()
+
